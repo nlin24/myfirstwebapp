10759b2a2fc9921b93ad93d1754159bdc64359f9
Nathan <nathan@example.com>
2025-06-20 16:12:00 -0500

    chap3 step4-1 add HTML via string buffer

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -6,9 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SayHelloController {
 //say-hello => "Hello! What are you learning today?"
-	
+
 	@GetMapping(path = "/say-hello")
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
+	
+	@GetMapping(path = "/say-hello-html")
+	public String sayHelloHtml() {
+		/*
+		 * <html>
+		 * <head><title>My first HTML page</title></head>
+		 * <body>My first HTML page with body</body>
+		 * </html>
+		 * */
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head><title>My first HTML page</title></head>");
+		sb.append("<body>My first HTML page with body</body>");
+		sb.append("</html>");
+		return sb.toString();
+	}
+
 }
