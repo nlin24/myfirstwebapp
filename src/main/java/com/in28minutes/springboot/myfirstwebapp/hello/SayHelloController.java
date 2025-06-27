e82a9a819e8f2c12eca5954979ca20b9a3e703a0
Nathan <nathan@example.com>
2025-06-26 23:27:56 -0500

    Add request mapping to JSP page

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,18 +1,24 @@
 package com.in28minutes.springboot.myfirstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SayHelloController {
 //say-hello => "Hello! What are you learning today?"
 
-	@GetMapping(path = "/say-hello")
+	@RequestMapping("/say-hello")
+	@ResponseBody
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
 	
-	@GetMapping(path = "/say-hello-html")
+	@RequestMapping("/say-hello-html")
+	@ResponseBody
 	public String sayHelloHtml() {
 		/*
 		 * <html>
@@ -27,5 +33,9 @@ public class SayHelloController {
 		sb.append("</html>");
 		return sb.toString();
 	}
-
+	
+	@RequestMapping("/say-hello-jsp")
+	public String sayHelloJsp() {
+		return "sayHello";
+	}
 }
