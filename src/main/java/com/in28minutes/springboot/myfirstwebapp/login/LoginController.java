e62bcb5924bd750c4058da7457d366cb0e7c7ff3
Nathan <nathan@example.com>
2025-07-06 09:14:05 -0500

    chap 3 step 8 add request params to login page

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,16 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping("/login")
-	public String loginJsp(){
+	public String loginJsp(@RequestParam String name, ModelMap loginM){
+		loginM.put("name", name);
 		return "login";
 	}
 
