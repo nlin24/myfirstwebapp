819105758a64c42bb5a58878fda3ccff43e4bc2a
Nathan <nathan@example.com>
2025-07-06 10:47:19 -0500

    chap3 step13 add simple authenticate service and to Login Controller with a fielded constructor

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
 	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private AuthenticationService authenticationService;
+		
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
 	
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String gotoLoginJsp(){
@@ -18,8 +27,18 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/login", method = RequestMethod.POST)
-	public String gotoWelcomePage(@RequestParam String name, ModelMap model){
+	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
 		model.put("name", name);
-		return "welcome";
+
+		// demo authentication
+		// name - in28minutes
+		// password - dummy
+		// redirect to welcome if authenticated, else back to login page
+
+		if (authenticationService.authenticate(name, password)) {
+			return "welcome";
+		}
+		model.put("errorMsg", "Invalid credential. Please try again.");
+		return "login";
 	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,10 +4,12 @@
 	</head>
 	<body>
 		Welcome to the login page!
+		<pre>${errorMsg}</pre>
 		<form method="post">
 			Name: <input type="text" name="name">
 			Password: <input type="password" name="password">
 			<input type="Submit">
+
 		</form>
 	</body>
 </html>
