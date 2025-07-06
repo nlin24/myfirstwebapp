f59f7a1887ef1177a5c58020b76d4e73619d0037
Nathan <nathan@example.com>
2025-07-06 10:02:37 -0500

    chap3 step12 add form input to the login flow

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,17 +1,25 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	
-	@RequestMapping("/login")
-	public String loginJsp(@RequestParam String name, ModelMap loginM){
-		loginM.put("name", name);
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String gotoLoginJsp(){
 		return "login";
 	}
-
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name, ModelMap model){
+		model.put("name", name);
+		return "welcome";
+	}
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,11 @@
 		<title>Login</title>
 	</head>
 	<body>
-		Welcome to the login page, ${name}
+		Welcome to the login page!
+		<form method="post">
+			Name: <input type="text" name="name">
+			Password: <input type="password" name="password">
+			<input type="Submit">
+		</form>
 	</body>
 </html>
