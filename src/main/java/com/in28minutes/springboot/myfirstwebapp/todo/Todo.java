c132d0727e623a585adf1d9943b74400102bf2f0
Nathan <nathan@example.com>
2025-07-17 00:15:00 -0500

    chap3 step15 Add todo list

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -0,0 +1,66 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+
+public class Todo {
+	private int id;
+	private String description;
+	private String username;
+	private LocalDate targetDate;
+	private boolean done;
+
+	public Todo(int id, String description, String username, LocalDate targetDate, boolean done) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.username = username;
+		this.targetDate = targetDate;
+		this.done = done;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", description=" + description + ", username=" + username + ", targetDate="
+				+ targetDate + ", done=" + done + "]";
+	}
+}
---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -0,0 +1,27 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TodoController {
+
+	private TodoService todoService;
+
+	public TodoController(TodoService todoService) {
+		super();
+		this.todoService = todoService;
+	}
+
+	// list-todos
+	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
+	public String listAllTodos(ModelMap model) {
+		List<Todo> todos = todoService.findByUsername("in28minutes");
+		model.put("todos", todos);
+		return "listTodos";
+	}
+}
