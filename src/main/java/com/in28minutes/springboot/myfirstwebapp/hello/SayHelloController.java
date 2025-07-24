d96f64dc8c455f750f8e3b91ca5926ecb35bea82
Nathan <nathan@example.com>
2025-07-24 13:16:10 +0800

    Remove unused imports

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -2,12 +2,9 @@ package com.in28minutes.springboot.myfirstwebapp.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SayHelloController {
