3477e4f608257bb3b8a23e111ced32e9711b348d
Nathan <nathan@example.com>
2025-07-31 15:42:31 +0800

    chap3 step23 add validation to todo controller and set binding result to todo jsp

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -41,7 +41,10 @@ public class TodoController {
 	}
 	
 	@RequestMapping(value="/add-todo", method=RequestMethod.POST)
-	public String addNewTodo(ModelMap model, Todo todo) {
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		if(result.hasErrors()) {
+			return "todo";
+		}
 		String username = (String) model.get("name");
 		LocalDate dueDate = LocalDate.now().plusYears(1);
 		todoService.addTodo(username, todo.getDescription(), dueDate, false);
