4891cb01bfdd6ee539b31570fdaca58c192e0c7a
Nathan <nathan@example.com>
2025-08-15 22:18:30 -0500

    chap2 step32 get username from Spring Security

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,12 @@ public class WelcomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String gotoWelcomePage(ModelMap model) {
-		model.put("username", "in28minutes");
+		model.put("username", getLoggedinUsername());
 		return "welcome";
+	}
+	
+	private String getLoggedinUsername() {
+		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 }
